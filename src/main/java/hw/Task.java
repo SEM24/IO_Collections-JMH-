6f19b12331ebc49f97cf06a0419d14d2b2c6ed27
SEM <samerx777222@gmail.com>
2021-04-10 22:08:33 +0300

    Initial Commit

---
+++ src/main/java/hw/Task.java
@@ -0,0 +1,10 @@
+package hw;
+
+public class Task {
+//    Find the maximum length of a line in a text file, consider the delimiter '\ n'.
+//
+//
+//  - Suggest the two most optimal solutions and add a comparative benchmark
+//
+//  - When loading data, remember that files can be large...
+}
